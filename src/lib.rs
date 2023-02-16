13dedef5986711e0b7d13d510b245f03e9f45386
bontaramsonta <18cs113@gmail.com>
2023-02-17 00:01:32 +0530

    add: lib get_space_seperated_input is generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
+use std::fmt::Debug;
 use std::io;
 use std::str::FromStr;
 
 pub fn get_input<T: FromStr>() -> Result<T, T::Err> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
     input.trim().parse::<T>()
 }
 
-pub fn get_space_seperated<T: FromStr>() -> Vec<i32> {
+pub fn get_space_separated<T: FromStr>() -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    input.trim().split_ascii_whitespace().map(|str| str.parse::<i32>().expect("failed to parse")).collect()
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse::<T>().expect("Failed to parse"))
+        .collect()
 }
