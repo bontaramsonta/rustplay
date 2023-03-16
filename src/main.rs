36b1295d7027ceaa5ee6164cdb863bdca27faf60
bontaramsonta <18cs113@gmail.com>
2023-03-16 13:08:38 +0530

    solve: leetcode merge two sorted arrays

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,38 @@ fn main() {
 
 fn solve(_a: usize) {
     println!("CASE: {_a}");
-    // let jewels = get_input::<String>().unwrap();
-    // let stone = get_input::<String>().unwrap();
-    let v = get_space_separated::<i32>();
-    let result = smaller_numbers_than_current(v);
+    let n = get_input::<i32>().unwrap();
+    let m = get_input::<i32>().unwrap();
+    let mut v1 = get_space_separated::<i32>();
+    let mut v2 = get_space_separated::<i32>();
+    let result = merge(&mut v1, m, &mut v2, n);
     println!("---------- {result:#?}");
 }
 
-pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-    nums.iter()
-        .enumerate()
-        .map(|(i, number)| {
-            nums.iter().enumerate().skip_while(|(j, _)| *j == i).fold(
-                0,
-                |acc, (_, other_number)| match number.cmp(&other_number) {
-                    std::cmp::Ordering::Greater => acc + 1,
-                    _ => acc,
-                },
-            )
-        })
-        .collect()
+pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    let mut result: Vec<i32> = Vec::new();
+    let mut i = 0;
+    let mut j = 0;
+    while i < m && j < n {
+        let val1 = nums1[i as usize];
+        let val2 = nums2[j as usize];
+        result.push(match val1.cmp(&val2) {
+            std::cmp::Ordering::Equal | std::cmp::Ordering::Less => {
+                i += 1;
+                val1
+            }
+            std::cmp::Ordering::Greater => {
+                j += 1;
+                val2
+            }
+        });
+    }
+    if i < m {
+        result.extend_from_slice(&nums1[(i as usize)..]);
+    }
+    if j < n {
+        result.extend_from_slice(&nums2[(j as usize)..]);
+    }
+    *nums1 = result;
+    nums1.iter().for_each(|r| println!("{r}"))
 }
