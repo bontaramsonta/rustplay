3f25936765b68d1d57815a6a9c0d50af18562d7a
bontaramsonta <18cs113@gmail.com>
2023-04-26 19:14:36 +0530

    solve: leetcode Symetric tree

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use std::{cell::RefCell, rc::Rc};
-
 // cargo watch -w src/main.rs -w in.txt -cqs 'cargo -q run < in.txt > out.txt'
 use rust_play::*;
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+struct TreeNode {
+    val: i32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-    fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
+    fn new(
+        val: i32,
+        left: Option<Rc<RefCell<TreeNode>>>,
+        right: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Self {
+        TreeNode { val, left, right }
     }
 }
 
@@ -29,36 +29,35 @@ fn main() {
 
 fn solve(_a: usize) {
     println!("CASE: {_a}");
-    // let n = get_input::<i32>().unwrap();
-    // let inorder = get_space_separated::<i32>();
-    // let postorder = get_space_separated::<i32>();
-    let first = Some(Rc::new(RefCell::new(TreeNode {
-        val: 1,
-        left: Some(Rc::new(RefCell::new(TreeNode::new(2)))),
-        right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
-    })));
-
-    let second = Some(Rc::new(RefCell::new(TreeNode {
-        val: 1,
-        left: Some(Rc::new(RefCell::new(TreeNode::new(2)))),
-        right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
-    })));
-
-    // let result = build_tree(inorder, postorder);
-    let result = is_same_tree(first, second);
-    println!("---------- {result:#?}");
+    let root = TreeNode::new(
+        1,
+        Some(Rc::new(RefCell::new(TreeNode::new(2, None, None)))),
+        Some(Rc::new(RefCell::new(TreeNode::new(2, None, None)))),
+    );
+    // dbg!(root);
+    dbg!(is_symmetric(Some(Rc::new(RefCell::new(root)))));
 }
 
-pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
-    match (p, q) {
+fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    if let None = root {
+        true
+    } else {
+        let root = root.unwrap();
+        let root = root.borrow();
+        is_mirror(root.left.as_ref(), root.right.as_ref())
+    }
+}
+
+fn is_mirror(t1: Option<&Rc<RefCell<TreeNode>>>, t2: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+    match (t1, t2) {
         (None, None) => true,
-        (Some(_), None) | (None, Some(_)) => false,
-        (Some(p), Some(q)) => {
-            let p = p.borrow();
-            let q = q.borrow();
-            p.val == q.val
-                && is_same_tree(p.left.clone(), q.left.clone())
-                && is_same_tree(p.right.clone(), q.right.clone())
+        (Some(t1), Some(t2)) => {
+            let t1 = t1.borrow();
+            let t2 = t2.borrow();
+            t1.val == t2.val
+                && is_mirror(t1.left.as_ref(), t2.right.as_ref())
+                && is_mirror(t1.right.as_ref(), t2.left.as_ref())
         }
+        _ => false,
     }
 }
