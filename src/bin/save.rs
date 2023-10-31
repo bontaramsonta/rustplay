534613aed2b612649641ac2aa8b8d247318e82d6
bontaramsonta <18cs113@gmail.com>
2023-10-31 22:30:45 +0530

    Rectangle struct impl Area trait

--- src/bin/save.rs
+++
@@ -1,5 +0,0 @@
-// cargo install --bin save --path .
-fn main() {
-  println!("save binary ran WIP!");
-  todo!("CLI to save playground");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,64 @@
-// cargo watch -w src/main.rs -w in.txt -cqs 'cargo -q run < in.txt > out.txt 2>&1'
-use rust_play::*;
-
-#[derive(Debug)]
-struct Fibonacci {
-    n: usize,
-    dp: Vec<i64>,
+struct Rectangle {
+    x: i32,
+    y: i32,
+    height: u32,
+    width: u32,
 }
-impl Fibonacci {
-    fn new(n: usize) -> Self {
-        let mut dp = vec![-1; n];
-        dp[0] = 0;
-        dp[1] = 1;
-        Self { n, dp }
+trait Area {
+    fn area(&self) -> u32;
+}
+
+impl Rectangle {
+    fn new(height: u32, width: u32) -> Rectangle {
+        Rectangle {
+            x: 0,
+            y: 0,
+            height,
+            width,
+        }
     }
-    fn get_fib(&mut self, n: usize) -> i64 {
-        let n = n - 1;
-        if n >= self.n {
-            // resize dp
-            self.dp.resize(n + 1, -1);
-            self.n = n + 1;
-        }
-        if self.dp[n] != -1 {
-            // got cached value
-            return self.dp[n];
-        }
-        self.dp[n] = self.get_fib(n) + self.get_fib(n - 1);
-        self.dp[n]
+    fn square(size: u32) -> Rectangle {
+        Rectangle::new(size, size)
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height >= other.height && self.width >= other.width
+    }
+    fn contains(&self, other: &Rectangle) -> bool {
+        self.x <= other.x
+            && self.y <= other.y
+            && (self.x as i32 + self.width as i32) >= (other.x as i32 + other.width as i32)
+            && (self.y as i32 + self.height as i32) >= (other.y as i32 + other.height as i32)
+    }
+    fn get_coords(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+    fn set_coords(&mut self, coords: (i32, i32)) {
+        self.x = coords.0;
+        self.y = coords.1;
+    }
+}
+
+impl Area for Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
     }
 }
 fn main() {
-    let test_cases = get_input::<usize>().unwrap();
-    let mut fib = Fibonacci::new(5);
-    for i in 0..test_cases {
-        solve(i, &mut fib);
-    }
-}
-
-fn solve(a: usize, fib: &mut Fibonacci) {
-    println!("CASE: {}", a + 1);
-    let n = get_input::<usize>().unwrap();
-    let result = fib.get_fib(n);
-    dbg!(n, result);
+    let mut rect1 = Rectangle::new(20, 20);
+    // get coords
+    let (mut x, mut y) = rect1.get_coords();
+    // update coords
+    x += -20;
+    y += -20;
+    rect1.set_coords((x, y));
+    let (x, y) = rect1.get_coords();
+    dbg!(x, y);
+    // create a square
+    let square = Rectangle::square(10);
+    // can rect1 hold square?
+    let can_hold = rect1.can_hold(&square);
+    assert_eq!(can_hold, true, "rect1 can hold square");
+    // does rect1 contain square?
+    let contains = rect1.contains(&square);
+    assert_eq!(contains, true, "rect1 contains square");
 }
